b59fd7958819c7ae497e1cd935bbe7f8e62170a1
Josafá <desenvolvedorjosafa@gmail.com>
2017-07-28 20:42:57 +0000

    fixed article

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,10 @@ class ApplicationController < ActionController::Base
   
   protected
   def configure_permitted_parameters
-    update_attrs = [:article_aproved, :article_title,:author_one,:author_two,:author_three, :name, :cpf, :matriculation, :participation_id, :theme_id, :course_id, :article]
+    update_attrs = [:author_one_cpf, :author_two_cpf, :author_three_cpf,
+    :article_aproved, :article_title,:author_one,:author_two,:author_three,
+    :name, :cpf, :matriculation, :participation_id, :theme_id, :course_id, :article]
+    
     devise_parameter_sanitizer.permit :account_update, keys: update_attrs
     devise_parameter_sanitizer.permit :sign_up, keys: update_attrs
   end
