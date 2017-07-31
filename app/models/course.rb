86a823c5ef57077250f716afa1df83ac2bbdc775
Josafá <desenvolvedorjosafa@gmail.com>
2017-07-31 02:15:31 +0000

    add total de vagas

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,11 @@
 class Course < ActiveRecord::Base
     
+    has_many :users
+    
     def self.avalaible
-        Course.all
+        courses = User.group(:course).count
+        courses = courses.map{|key,_| key if courses[key] <=35}.compact
+        courses << Course.first
     end
     
     def members_length
