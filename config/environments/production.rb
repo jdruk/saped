51e6f3476b43acecbd25e4e2ee0e2a0f313a8c97
Josafá <desenvolvedorjosafa@gmail.com>
2017-09-06 01:53:58 +0000

    teste1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,14 +79,13 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host:'saped.net', port: '80' }
+  config.action_mailer.default_url_options = { host:'saped.net' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
       :address => "smtp.gmail.com",
       :port => 587,
-      :domain => 'saped.net',
       :user_name => "devjosafa@gmail.com",
       :password => "Mjdruk101",
       :authentication => :plain,
