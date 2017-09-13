bb799503c2abbfba0bf4b3b73f21e9f85749bed7
Josafá <desenvolvedorjosafa@gmail.com>
2017-09-13 02:09:26 +0000

    t

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,18 +3,19 @@ class Course < ActiveRecord::Base
     has_many :users
     
     def self.avalaible
-        courses = []
+        c = Array.new
         minicurso_rociane = Course.where name: 'Arte e Preservação Ambiental – Beleza Permanente'
         minicurso_patricia = Course.where name: 'Aprender a aprender na prática: a neurociência da aprendizagem'
         Course.all.each do |course|
             if course.equal?(Course.first)
-                courses << course
+                c << course
             end
             if course.equal?(minicurso_rociane) or course.equal?(minicurso_patricia)
-                courses << valida(course, 18)
+                c << Course.valida(course, 18)
             end
-            courses << valida(course, 33)
+            c << Course.valida(course, 33)
         end
+        return c
     end
     
     def members_length
@@ -28,6 +29,8 @@ class Course < ActiveRecord::Base
     def self.valida course, quantidade
         if is_available_vacancies(course, quantidade)
             course
+        else 
+            nil
         end 
     end
     
